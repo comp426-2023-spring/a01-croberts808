6faaff2845d6858494ffe1aa8d7cb79f5a4e729f
croberts808 <crobert7@email.unc.edu>
2023-01-27 11:37:34 -0500

    added everything from tutorial, fixing bugs

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
-// Require http module
-var http = require('http');
-// Require fs module
-var fs = require('fs');
-// Require minimist module (make sure you install this one via npm).
-var mini = require('minimist');
-// Use minimist to process one argument `--port=` on the command line after `node server.js`.
+    // Require http module
+const http = require('http');
+    // Require fs module
+const fs = require('fs');
+    // Require minimist module (make sure you install this one via npm).
+const mini = require('minimist');
+    // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 const args = mini(process.argv.slice(2))
-// Define a const `port` using the argument from the command line. 
-// Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args.port || 3000;
-// Use the fs module to create an arrow function using `fs.readFile`.
-// Use the documentation for the Node.js `fs` module. 
-// The function must read a file located at `./public/index.html` and do some stuff with it.
-// The stuff that should be inside this function is all below.
+    // Define a const `port` using the argument from the command line. 
+    // Make this const default to port 3000 if there is no argument given for `--port`.
+const port = args.port || 3000; 
+    // Use the fs module to create an arrow function using `fs.readFile`.
+    // Use the documentation for the Node.js `fs` module. 
+    // The function must read a file located at `./public/index.html` and do some stuff with it.
+    // The stuff that should be inside this function is all below.
 
-// If there is an error, put it on the console error and return. 
-// Do not be nice about exiting.
+    // If there is an error, put it on the console error and return. 
+    // Do not be nice about exiting.
 
 
 
+
+const hostname = '127.0.0.1';
+
+
+
+fs.readFile('./public/index.html', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
 
 
 // Define a const `server` as an arrow function using http.createServer. 
@@ -28,14 +40,18 @@ const port = args.port || 3000;
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 
-
-
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('text/html', 'text/plain');
+    res.end('./public/index.html');
+  });
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
-
-
+server.listen(port, hostname, () => {
+    console.log(`Server listening on port ${port}`);
+  });
 
 // That's it! You're all done!
